aae40991b5358897699c2461c6eaa0fb1b9451e2
AronT <aron.thompson@racunarstvo.hr>
2020-05-24 01:58:19 +0200

    Upload with both blends and regular animations

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger(PlayerAnimationParameters.jumpTrigger);
+        }
+
+        var x = (Input.GetAxis("Vertical"));
+        anim.SetFloat(PlayerAnimationParameters.speedFloat, x);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            print("Axis = " + x);
+        }
+    }
+}
+
+struct PlayerAnimationParameters
+{
+    public static string jumpTrigger = "jump";
+    public static string walkingBool = "isWalking";
+    public static string runningBool = "isRunning";
+    public static string speedFloat = "speed";
+}
